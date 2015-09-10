2cbe5679f99d7da50b8221a52da83332c58fb790
Tyler Machen <tylermachen@gmail.com>
2015-09-10 18:04:46 -0400

    done

--- js/selectors.js
+++ js/selectors.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
   checkboxInputSelector();
 });
 
-// declare your functions here...
+function h1Selector(){
+  return $('h1');
+};
+
+function liInOlSelector(){
+  return $('ol li');
+};
+
+function linkSelector(){
+  return $('#box4.box5 a');
+};
+
+function imageSelector(){
+  return $("img[alt='cat sleeping']");
+};
+
+function checkboxInputSelector(){
+  return $('input:checkbox').wrap('<span class="pinky"></span>');
+};
